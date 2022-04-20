1c6d38ec31cf66abd7e6be8d00dd9dbbd2fc3aad
toby-a <tawender@customsiliconsolutions.com>
2022-04-20 14:29:21 -0700

    In vitis created new platform, application projects for latest xsa for fpga updates

--- vitis/design_2_wrapper/platform.tcl
+++ vitis/design_2_wrapper/platform.tcl
@@ -1,16 +1,16 @@
 # 
 # Usage: To re-create this platform project launch xsct with below options.
-# xsct C:\Xilinx_projects\gyro2tester\vitis\design_2_wrapper\platform.tcl
+# xsct C:\css\ngc\gyro2\gyro2tester\vitis\design_2_wrapper\platform.tcl
 # 
 # OR launch xsct and run below command.
-# source C:\Xilinx_projects\gyro2tester\vitis\design_2_wrapper\platform.tcl
+# source C:\css\ngc\gyro2\gyro2tester\vitis\design_2_wrapper\platform.tcl
 # 
 # To create the platform in a different location, modify the -out option of "platform create" command.
 # -out option specifies the output directory of the platform project.
 
 platform create -name {design_2_wrapper}\
--hw {C:\Xilinx_projects\gyro2tester\vivado\project\design_2_wrapper.xsa}\
--proc {ps7_cortexa9_0} -os {standalone} -out {C:/Xilinx_projects/gyro2tester/vitis}
+-hw {C:\css\ngc\gyro2\gyro2tester\vivado\project\design_2_wrapper.xsa}\
+-proc {ps7_cortexa9_0} -os {standalone} -out {C:/css/ngc/gyro2/gyro2tester/vitis}
 
 platform write
 platform generate -domains 
